1b22d012973f148fa050c7d8526ef12089c4e004
Matt Steedman <steedman87@gmail.com>
2013-10-30 12:12:42 -0400

    initial commit

---
+++ render-bug.js
@@ -0,0 +1,27 @@
+Messages = new Meteor.Collection('messages')
+
+if (Meteor.isClient) {
+
+  Template.main.messages = function () {
+    if (Meteor.user()) {
+      return Messages.find({})
+    } else {
+      return []
+    }
+  };
+
+  Template.main.events({
+    'click [type="button"]' : function () {
+      if (Meteor.user().profile.foo === 'bar') {
+        Meteor.users.update(Meteor.userId(), { $set: {'profile.foo': 'baz'} })
+      } else {
+        Meteor.users.update(Meteor.userId(), { $set: {'profile.foo': 'bar'} })
+      }
+    },
+    'submit #new-message' : function (e, t) {
+      e.preventDefault();
+      Messages.insert({text: t.find('[type="text"]').value});
+      t.find('[type="text"]').value = ''
+    }
+  });
+}
