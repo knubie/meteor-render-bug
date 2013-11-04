73e0f9dac20eb92bf234e52be0b669be3d48e05f
Matt Steedman <steedman87@gmail.com>
2013-11-04 09:40:49 -0500

    updated to show new bug(s).

--- render-bug.js
+++ render-bug.js
@@ -1,29 +1,68 @@
 Messages = new Meteor.Collection('messages')
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Messages.find().count() === 0) {
+      Messages.insert({set: 'a', text: 'one'});
+      Messages.insert({set: 'a', text: 'two'});
+      Messages.insert({set: 'a', text: 'three'});
+      Messages.insert({set: 'b', text: '1'});
+      Messages.insert({set: 'b', text: '2'});
+      Messages.insert({set: 'b', text: '3'});
+    }
+  });
+}
 
 if (Meteor.isClient) {
 
+  Session.setDefault('foo', 'one');
+  Session.setDefault('set', 'a');
+  Session.setDefault('page', 'main');
+
+  // This gets called too many times.
+  Template.main.data = function () {
+    console.log('getting foo.');
+    return Session.get('foo');
+  }
+
   Template.main.messages = function () {
-    if (Meteor.user()) {
-      return Messages.find({})
-    } else {
-      return []
-    }
+    return Messages.find({set: Session.get('set')})
   };
 
   Template.main.events({
-    'click [type="button"]' : function () {
-      if (! Meteor.user().profile) {
-        Meteor.users.update(Meteor.userId(), { $set: {'profile.foo': 'bar'} })
-      } else if (Meteor.user().profile.foo === 'bar') {
-        Meteor.users.update(Meteor.userId(), { $set: {'profile.foo': 'baz'} })
+    'click input' : function (e, t) {
+      console.log('-----switch set-----');
+      if (Session.equals('set', 'a')) {
+        Session.set('set', 'b');
       } else {
-        Meteor.users.update(Meteor.userId(), { $set: {'profile.foo': 'bar'} })
+        Session.set('set', 'a');
       }
-    },
-    'submit #new-message' : function (e, t) {
-      e.preventDefault();
-      Messages.insert({text: t.find('[type="text"]').value});
-      t.find('[type="text"]').value = ''
+    }
+  });
+
+  Template.message.helpers({
+    selected: function () {
+      if (Session.get('foo') === this.text) {
+        return 'selected';
+      } else {
+        return '';
+      }
+    }
+  });
+
+  Template.message.rendered = function () {
+    console.log('message rendered.');
+  }
+
+  Template.message.events({
+    'click li' : function (e, t) {
+      console.log('+++++switch session+++++');
+      Session.set('foo', e.currentTarget.innerText);
+    }
+  });
+
+  Handlebars.registerHelper('page', function (page) {
+    if (Session.equals('page', page)) {
+     return Template._page;
     }
   });
 }
